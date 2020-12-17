0295c2a5ee689a79a0c670a0def6ab57679277e7
nzekenov <nzekenov@andrew.cmu.edu>
2020-12-17 19:17:30 +0600

    try again

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,5 +1,8 @@
 def reverse_dict(dict):
     # swap keys and values within dict and return dict
-    dict = {v:k for (k,v) in dict.items()}
-  return dict
+    for key in (dict.keys()):
+      temp = dict.get(key)
+      dict[key] = key
+      key = temp
+    return dict
 
