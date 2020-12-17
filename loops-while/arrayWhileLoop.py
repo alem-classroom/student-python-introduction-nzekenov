64af2435208ac658205aad45cd50d0fce9395530
nzekenov <nzekenov@andrew.cmu.edu>
2020-12-17 20:07:07 +0600

    while loop

--- loops-while/arrayWhileLoop.py
+++ loops-while/arrayWhileLoop.py
@@ -1,7 +1,9 @@
 def insert_squares(arr, num):
     # add square of numbers from 1 to num to the list named arr and return list
     n = num
+    diff = 0
     while (n > 0):
-      arr.append((num - n + 1) * (num - n + 1))
+      diff = num - n
+      arr.append((diff + 1) * (diff + 1))
       n -= 1
     return arr
