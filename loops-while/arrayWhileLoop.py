863640058a6ea4f3ec877cc53ce75a962136db62
nzekenov <nzekenov@andrew.cmu.edu>
2020-12-17 19:58:40 +0600

    fixed infinite loop

--- loops-while/arrayWhileLoop.py
+++ loops-while/arrayWhileLoop.py
@@ -2,6 +2,6 @@ def insert_squares(arr, num):
     # add square of numbers from 1 to num to the list named arr and return list
     n = num
     while (n > 0):
-      arr.append((n - num + 1) * (n - num + 1))
+      arr.append((num - n + 1) * (num - n + 1))
       n -= 1
     return arr
